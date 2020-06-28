4f6fce7426daade4be0bffe0221ea255b42af6cf
Gabriele Gilardi <gilardi.gabriele@gmail.com>
2020-06-28 11:40:18 +0900

    add comments

--- Code_Python/test.py
+++ Code_Python/test.py
@@ -8,14 +8,14 @@ References
 ----------
 
 - John F. Ehlers, "Cycle Analytics for Traders: Advanced Technical Trading
-  Concepts", @ http://www.mesasoftware.com/ehlers_books.htm.
+  Concepts", @ www.mesasoftware.com/ehlers_books.htm.
 
 - D. Prichard and J. Theiler, "Generating surrogate data for time series with
   several simultaneously measured variables",
-  @ https://journals.aps.org/prl/abstract/10.1103/PhysRevLett.73.951.
+  @ journals.aps.org/prl/abstract/10.1103/PhysRevLett.73.951.
 
 - H. Vinod and J. Lopez-de-Lacalle, "Maximum entropy bootstrap for time series:
-  the meboot R package, @ https://www.jstatsoft.org/article/view/v029i05.
+  the meboot R package, @ www.jstatsoft.org/article/view/v029i05.
 
 Characteristics
 ---------------
